fbae66652b751b68008f59f466d7921c9a91c014
Steve Miller <smiller@mathworks.com>
2019-11-02 15:24:35 +0100

    Initial commit R2019b.  Converted to MATLAB project with reference project for core library.

---
+++ Scripts_Data/sm_ssci_publish_all.m
@@ -0,0 +1,27 @@
+cd(fileparts(which('sm_ssci_01_slider_crank.slx')));
+cd('html')
+publish_all_html
+
+cd(fileparts(which('sm_lift_table_0_import.slx')));
+cd('html');
+publish_all_html
+
+cd(fileparts(which('sm_ssci_electric_lift_trsn.slx')));
+cd('html');
+publish_all_html
+
+% Copyright 2016-2019 The MathWorks(TM), Inc.
+
+function publish_all_html
+filelist_m=dir('*.m');
+
+filenames_m = {filelist_m.name};
+
+warning('off','Simulink:Engine:MdlFileShadowedByFile');
+
+for i=1:length(filenames_m)
+    if ~(strcmp(filenames_m{i},'publish_all_html.m'))
+    publish(filenames_m{i},'showCode',false)
+    end
+end
+end
---
+++ Scripts_Data/startup_Multibody_Multiphysics_Library.m
@@ -0,0 +1,11 @@
+function startup_Multibody_Multiphysics_Library
+% Copyright 2016-2019 The MathWorks, Inc.
+
+curr_proj = simulinkproject;
+cd(curr_proj.RootFolder);
+
+if(curr_proj.Information.TopLevel)
+    if (exist('Multibody_Multiphysics_Demo_Script.html','file'))
+        web('Multibody_Multiphysics_Demo_Script.html');
+    end
+end
